da84543c00c74e90c568fab2d648ff9134d1a135
Bishal Chettri <bishalin01@gmail.com>
2023-11-17 14:40:49 +0530

    initialisation check

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from src.services import home
 from src.common import filepaths, prompts
 from src.loggers.general_logger import GeneralLogger
 from src.models.db_connection import DBConnection
+from src.utils.initialisation import initialize
 
 if __name__ == "__main__":
     GeneralLogger.info(prompts.SYSTEM_START, filepaths.SYSTEM_LOG_FILE)
     try:
+        initialize()
         home.home_menu()
 
     except Exception as exc:
--- src/utils/initialisation.py
+++ src/utils/initialisation.py
@@ -1,12 +1,14 @@
 from src.common.sql_query import Sql
 from src.utils import database
 
-database.single_query(Sql.CREATE_USER_TABLE.value)
 
-database.single_query(Sql.CREATE_BLOG_TABLE.value)
+def initialize():
+    database.single_query(Sql.CREATE_USER_TABLE.value)
 
-database.single_query(Sql.CREATE_COMMENTS_TABLE.value)
+    database.single_query(Sql.CREATE_BLOG_TABLE.value)
 
-database.single_query(Sql.CREATE_BLOG_UPVOTES_TABLE.value)
+    database.single_query(Sql.CREATE_COMMENTS_TABLE.value)
 
-database.single_query(Sql.CREATE_COMMENT_UPVOTES_TABLE.value)
+    database.single_query(Sql.CREATE_BLOG_UPVOTES_TABLE.value)
+
+    database.single_query(Sql.CREATE_COMMENT_UPVOTES_TABLE.value)
