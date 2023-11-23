cd6a6c59bb27c4322e15e940e59b27c426a47db3
Bishal Chettri <bishalin01@gmail.com>
2023-11-23 10:48:13 +0530

    ok

--- testing/test_utils/test_validation/test_validate_username.py
+++ testing/test_utils/test_validation/test_validate_username.py
@@ -4,32 +4,29 @@ import re
 from src.utils.validation import validate_username
 
 
-def check_username(input_val, expected_val):
-    val_received = validate_username(input_val)
-    assert val_received == expected_val
+class TestUsername:
+    def check_username(self, input_val, expected_val):
+        val_received = validate_username(input_val)
+        assert val_received == expected_val
 
+    def test_empty_input(self):
+        self.check_username('', False)
 
-def test_empty_input():
-    check_username('', False)
+    def test_invalid_text(self):
+        self.check_username('2##12', False)
 
+    def test_number_input(self):
+        self.check_username('1231231', False)
 
-def test_invalid_text():
-    check_username('2##12', False)
-
-
-def test_number_input():
-    check_username('1231231', False)
-
-
-@pytest.mark.parametrize("username, result",
-                         [
-                             ("bishal", True),
-                             ("snow123", True),
-                             ("test222", True),
-                             ("_test12", True),
-                             ("_test_", True),
-                             ("_test_1_2", True)
-                         ])
-def test_valid_input(username, result):
-    print(f"testing `{username}`")
-    check_username(username, result)
+    @pytest.mark.parametrize("username, result",
+                             [
+                                 ("bishal", True),
+                                 ("snow123", True),
+                                 ("test222", True),
+                                 ("_test12", True),
+                                 ("_test_", True),
+                                 ("_test_1_2", True)
+                             ])
+    def test_valid_input(self, username, result):
+        print(f"testing `{username}`")
+        self.check_username(username, result)
