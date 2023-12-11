12e4e56570f8447be3f4743e6ea289526b589d4d
Bishal Chettri <bishalin01@gmail.com>
2023-12-11 16:37:23 +0530

    added tests for blogger menu

--- testing/test_views/test_blogger_view/test_blogger_view.py
+++ testing/test_views/test_blogger_view/test_blogger_view.py
@@ -1,8 +1,7 @@
 import pytest
 from unittest.mock import patch, Mock
-from src.helpers.blogger import take_input
-from src.views import blogger
 from src.views.blogger import blogger_menu
+from src.views.blogger import prompts
 
 
 class TestBloggerView:
@@ -23,7 +22,7 @@ class TestBloggerView:
             mock_view_blogs.return_value = True  # only for testing
             blogger_menu(mock_user)
 
-        mock_view_blogs.assert_called()
+        mock_view_blogs.assert_called_once()
 
     @pytest.mark.parametrize('choices', [('2', '11')])
     def test_choice_2(self, choices, monkeypatch, mock_take_input, mock_user):
@@ -36,4 +35,107 @@ class TestBloggerView:
             mock_view_blogs_by_user.return_value = True
             blogger_menu(mock_user)
 
-        mock_view_blogs_by_user.assert_called()
+        mock_view_blogs_by_user.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('3', '11')])
+    def test_choice_3(self, choices, monkeypatch, mock_take_input, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        mock_take_input.get_tag_name.return_value = 'test_tag'
+
+        with patch('src.views.blogger.view_blogs_by_tag_name') as mock_view_blogs_by_tag_name:
+            mock_view_blogs_by_tag_name.return_value = True
+            blogger_menu(mock_user)
+
+        mock_view_blogs_by_tag_name.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('4', '11')])
+    def test_choice_4(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.view_one_blog') as mock_view_one_blog:
+            mock_view_one_blog.return_value = True
+            blogger_menu(mock_user)
+
+        mock_view_one_blog.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('5', '11')])
+    def test_choice_5(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.create_blog') as mock_create_blog:
+            mock_create_blog.return_value = True
+            blogger_menu(mock_user)
+
+        mock_create_blog.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('6', '11')])
+    def test_choice_6(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.edit_blog') as edit_blog:
+            edit_blog.return_value = True
+            blogger_menu(mock_user)
+
+        edit_blog.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('7', '11')])
+    def test_choice_7(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.remove_blog') as mock_remove_blog:
+            mock_remove_blog.return_value = True
+            blogger_menu(mock_user)
+
+        mock_remove_blog.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('8', '11')])
+    def test_choice_8(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.upvote_blog') as mock_upvote_blog:
+            mock_upvote_blog.return_value = True
+            blogger_menu(mock_user)
+
+        mock_upvote_blog.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('9', '11')])
+    def test_choice_9(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.comment_on_blog') as mock_comment_on_blog:
+            mock_comment_on_blog.return_value = True
+            blogger_menu(mock_user)
+
+        mock_comment_on_blog.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('10', '11')])
+    def test_choice_10(self, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        with patch('src.views.blogger.change_password') as mock_change_password:
+            mock_change_password.return_value = True
+            blogger_menu(mock_user)
+
+        mock_change_password.assert_called_once()
+
+    @pytest.mark.parametrize('choices', [('13', '11')])
+    def test_choice_10(self, capsys, choices, monkeypatch, mock_user):
+        choice = iter(choices)
+        monkeypatch.setattr('builtins.input', lambda _: next(choice))
+
+        blogger_menu(mock_user)
+
+        captured = capsys.readouterr()
+        assert captured.out.strip() in prompts.ENTER_VALID_CHOICE
+
+
+
