f0289ea731d1c6472080f9fd36ce76e47f866ad5
Bishal Chettri <bishalin01@gmail.com>
2023-12-18 12:17:10 +0530

    type hinting and docs

--- src/models/db_connection.py
+++ src/models/db_connection.py
@@ -1,9 +1,14 @@
+"""This module defines how db connection is created"""
+
 import sqlite3
 
 from src.config.filepaths import BLOGGING_DB
 
 
 class DBConnection:
+    """
+    This class contains methods to create a db context manager with singleton approach
+    """
     _instance = None
 
     def __new__(cls):
@@ -16,11 +21,17 @@ class DBConnection:
         return cls._instance
 
     def __enter__(self) -> sqlite3.Connection:
+        """
+        Setup operations when context manager is opened
+        """
         self.connection = sqlite3.connect(self.host)
         self.cursor = self.connection.cursor()
         return self.connection
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        """
+        Operations when the context manager is closed
+        """
         if exc_type or exc_val or exc_tb:
             print(f"Exception type: {exc_type}")
             print(f"Exception value: {exc_val}")
@@ -29,6 +40,9 @@ class DBConnection:
         else:
             self.connection.commit()
 
-    def close(self):
+    def close(self) -> None:
+        """
+        Explicitly close the database connection
+        """
         if self.connection:
             self.connection.close()
